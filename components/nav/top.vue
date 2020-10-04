89ef5dba59b3ad647b049a1b75bb009527eed72e
s.kusuhara <s.kusuhara@trustridge.jp>
2020-10-04 17:24:19 +0900

    first commit

---
+++ components/nav/top.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="topnav">
+    <div class="topnav__linkContent">about</div>
+    <div class="topnav__linkContent">ログイン</div>
+    <div class="topnav__linkContent">会員登録</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.topnav {
+  background-color: gray;
+  color: black;
+  display: flex;
+  flex-direction: row-reverse;
+  height: 3vh;
+  &__linkContent {
+    margin-right: 1vw;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <top />
+    <div>
+      <h1 class="title">jososta</h1>
+    </div>
+    <bottom />
+  </div>
+</template>
+
+<script>
+import top from '../components/nav/top'
+import bottom from '../components/nav/bottom'
+export default {
+  components: {
+    top,
+    bottom,
+  },
+}
+</script>
+
+<style>
+.container {
+  text-align: center;
+}
+</style>
